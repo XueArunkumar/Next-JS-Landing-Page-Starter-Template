1721f2047896f4cd37fcbda48ece33b857d1eb3e
XueArunkumar <154484699+XueArunkumar@users.noreply.github.com>
2023-12-21 13:55:02 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+/* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  eslint: {
+    dirs: ['.'],
+  },
   output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html'
+  poweredByHeader: false,
   trailingSlash: true,
-  restrictStrictMode: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
+});
